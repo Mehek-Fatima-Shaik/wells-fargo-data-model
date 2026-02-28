8bb7f1d994d8c1b4dcc4cebb21c2fe0e7322a1cf
Mehek-Fatima-Shaik <163736626+Mehek-Fatima-Shaik@users.noreply.github.com>
2026-02-28 17:44:27 +0530

    Add Customer entity with fields and methods

---
+++ Customer.java
@@ -0,0 +1,44 @@
+package com.wellsfargo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    public Customer() {}
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
